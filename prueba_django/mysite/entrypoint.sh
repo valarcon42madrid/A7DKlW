a2deed81ef926343f4fb858f2ed387baab9d94d4
valarcon42madrid <108015531+valarcon42madrid@users.noreply.github.com>
2024-03-13 18:49:17 +0100

    automatic staff and admin

--- prueba_django/mysite/entrypoint.sh
+++ prueba_django/mysite/entrypoint.sh
@@ -14,4 +14,8 @@ fi
 python manage.py flush --no-input
 python manage.py migrate
 
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass123')"
+
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('staff', 'staff@example.com', 'staffpass123', is_staff=True)"
+
 exec "$@"
